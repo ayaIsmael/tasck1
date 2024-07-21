ace3a160f55af199a1137d4b3d802e4125c6febd
ayaIsmael <124537490+ayaIsmael@users.noreply.github.com>
2024-07-21 16:25:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+let header=document.getElementById('header');
+let sections=document.querySelectorAll('section');
+let links=document.querySelectorAll('.nav-item .nav-link');
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 0){
+        header.classList.add('shrink');
+    }else{
+        header.classList.remove('shrink');
+    }
+
+    sections.forEach(section => {
+        if(window.scrollY >= section.offsetTop - 100 && window.scrollY < (section.offsetTop + section.scrollHeight - 100)){
+            links.forEach(link => {link.classList.remove('active')});
+            let link = document.getElementById(section.dataset.link);
+            link.classList.add('active');
+        }
+    });
+
+    
+});
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        responsive: {
+            0:{
+                items:1
+            },
+            567:{
+                items:2
+            },
+            992:{
+                items:4
+            }
+        },
+        dots: true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+    });
+});
